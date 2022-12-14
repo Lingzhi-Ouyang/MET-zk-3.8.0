b1b61772975d98815ed4d3bb7ed562699975bc1c
Lingzhi-Ouyang <lingzhi.ouyang@outlook.com>
2022-12-14 23:47:22 +0800

    add post-condition of releasing FollowerProcessNEWLEADERAfterCurrentEpochUpdated

--- zk-test/server/src/main/java/org/disalg/met/server/executor/LocalEventExecutor.java
+++ zk-test/server/src/main/java/org/disalg/met/server/executor/LocalEventExecutor.java
@@ -64,38 +64,56 @@ public class LocalEventExecutor extends BaseEventExecutor{
 
         // set the next subnode to be PROCESSING
         switch (subnodeType) {
-            case QUORUM_PEER: // for FollowerProcessSyncMessage && LeaderJudgingIsRunning
+            case QUORUM_PEER:
+                // follower: FollowerProcessSyncMessage / FollowerProcessPROPOSALInSync
+                //              / FollowerProcessCOMMITInSync / SubmitLoggingTaskInProcessingNEWLEADER
+                // leader: LeaderJudgingIsRunning
                 LeaderElectionState role = testingService.getLeaderElectionState(nodeId);
                 if (role.equals(LeaderElectionState.FOLLOWING)) {
-                    // for FollowerProcessSyncMessage: wait itself to SENDING state since ACK_NEWLEADER will come later anyway
-                    // for FollowerProcessCOMMITInSync:
-                    // FollowerProcessPROPOSALInSync:
                     int eventType = event.getType();
-                    if (eventType == MessageType.DIFF || eventType == MessageType.TRUNC || eventType == MessageType.SNAP) {
-                        testingService.getSyncTypeList().set(event.getNodeId(), event.getType());
+                    if (eventType == TestingDef.MessageType.NEWLEADER) { // release FollowerProcessNEWLEADERAfterCurrentEpochUpdated
+                        //  Post-condition: FollowerSendACKtoNEWLEADER by follower's QUORUM_PEER
+                        testingService.getControlMonitor().notifyAll();
+                        testingService.waitSubnodeTypeSending(nodeId, SubnodeType.QUORUM_PEER);
+
+                        // let leader's corresponding learnerHandler be intercepted at ReadRecord
+                        testingService.getControlMonitor().notifyAll();
+                        testingService.waitSubnodeInSendingState(testingService.getFollowerLearnerHandlerMap(nodeId));
                     }
-                    testingService.getControlMonitor().notifyAll();
-                    testingService.waitSubnodeInSendingState(testingService.getFollowerLearnerHandlerSenderMap(nodeId));
-                    testingService.getControlMonitor().notifyAll();
-                    testingService.waitSubnodeInSendingState(testingService.getFollowerLearnerHandlerMap(nodeId));
+                    else {
+                        // TODO: deprecated since this branch will not be triggered!
+                        LOG.warn("This branch is not supposed to occur!!! Local event: {}", event);
+                        if (eventType == MessageType.DIFF || eventType == MessageType.TRUNC || eventType == MessageType.SNAP) {
+                            testingService.getSyncTypeList().set(event.getNodeId(), event.getType());
+                        }
+                        // for FollowerProcessSyncMessage / FollowerProcessPROPOSALInSync / FollowerProcessCOMMITInSync:
+                        //  Post-condition:  wait for the follower's QUORUM_PEER thread to SENDING state
+                        //      since ACK_LD will come at last anyway
+                        testingService.getControlMonitor().notifyAll();
+                        testingService.waitSubnodeInSendingState(testingService.getFollowerLearnerHandlerSenderMap(nodeId));
+                        testingService.getControlMonitor().notifyAll();
+                        testingService.waitSubnodeInSendingState(testingService.getFollowerLearnerHandlerMap(nodeId));
+                    }
                 }
                 break;
-            case SYNC_PROCESSOR:
+            case SYNC_PROCESSOR: // leader / follower do log
                 final Long zxid = event.getZxid();
                 Map<Long, Integer> zxidSyncedMap = testingService.getZxidSyncedMap();
                 testingService.getZxidSyncedMap().put(zxid, zxidSyncedMap.getOrDefault(zxid, 0) + 1);
 
-//                 intercept log event and ack event
-//                 leader will ack self, which is not intercepted
-//                 follower will send ACK message to leader, which is intercepted only in follower
+                // for log event and ack event
+                // leader will ack self, which is not intercepted
+                // follower will send ACK message to leader, which is intercepted only in follower
                 LOG.debug("wait follower {}'s SYNC thread be SENDING ACK", event.getNodeId());
                 if (LeaderElectionState.FOLLOWING.equals(testingService.getLeaderElectionState(nodeId))
                         && event.getFlag() != TestingDef.RetCode.NO_WAIT) {
+                    // Post-condition: FollowerSendACKtoPROPOSAL by follower's SYNC_PROCESSOR
                     testingService.getControlMonitor().notifyAll();
                     testingService.waitSubnodeInSendingState(subnodeId); // this is just for follower
                 }
                 break;
-            case COMMIT_PROCESSOR:
+            case COMMIT_PROCESSOR:  // leader / follower do commit
+                // Post-condition: wait for the node update its lastProcessedZxid
                 testingService.getControlMonitor().notifyAll();
                 testingService.waitCommitProcessorDone(event.getId(), event.getNodeId());
                 break;
